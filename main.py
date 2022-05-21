63b2ddeeefbb43007d9715b3097e581a0a2e92b8
Yuri Silveira <ypereirars@gmail.com>
2022-05-21 19:51:38 -0300

    refactoring main function

--- main.py
+++ main.py
@@ -1,43 +1,80 @@
+import os
+import csv
+import json
+import argparse
 from nfce import NFCeParser, NfeScrapper
-import argparse, json, csv
 
 
 def main(args):
     parser = NFCeParser()
-    processor = NfeScrapper(parser, args.webdriver_path)
+    processor = NfeScrapper(parser, args.webdriver_path, wait_timeout=10)
     data = processor.get(args.url)
 
-    output_path = f'{args.out}.{args.format}'
+    if data is None:
+        print("No data found")
+        return 1
+
+    if args.output is None:
+        print(data.serialize())
+        return 0
+
+    output_path, output_format = get_output_path(args)
+
+    return export_data(data, output_path, output_format)
+
+
+def get_output_path(args):
+    _, ext = os.path.splitext(args.output)
+
+    if ext == '':
+        output_format = str(args.format).lower()
+        output_path = os.path.join(args.output, f'data.{args.format}')
+    else:
+        output_format = str(ext.strip('.')).lower()
+        output_path = args.output
+
+    return output_path, output_format
+
+
+def export_data(data, output_path, output_format):
     with open(output_path, 'w', encoding='utf-8') as outfile:
-        if args.format == "json":
+        if output_format == 'json':
             data = data.serialize()
             json.dump(data, outfile, ensure_ascii=False, indent=4)
-        elif args.format == "csv":
-            writer = csv.writer(outfile, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
-            rows = data.to_csv()
-            write_header = True
-            for header, row in rows:
-                if write_header:
-                    writer.writerow(header)
-                    write_header = False
+            return 0
+        elif output_format == 'csv':
+            dump_csv(data, outfile)
+            return 0
+        else:
+            print(f"Unknown format: {output_format}")
+            return 1
 
-                writer.writerow(row)
+
+def dump_csv(data, outfile):
+    writer = csv.writer(outfile, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
+    rows = data.to_csv()
+    write_header = True
+    for header, row in rows:
+        if write_header:
+            writer.writerow(header)
+            write_header = False
+
+        writer.writerow(row)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Download data from a NFe.')
 
-    parser.add_argument('--url',  metavar='url', type=str, required=True,
-                        help='URL to download NFe.')
+    parser.add_argument('url',  metavar='url', type=str, help='URL to download the NFe.')
+    parser.add_argument('--format', '-f', metavar='format', type=str, default='json', choices=['json', 'csv'],
+                        help='Output file format wich may either be json or csv.')
+    parser.add_argument('--output', '-o',  metavar='path', type=str, default=None,
+                        help='File path location where to save data. If path is a folder, than the output will be saved'
+                        ' in <path>/data.<format> where <format> is the same as the --format option. However if path is'
+                        ' a file, than the output will be saved in <path>.')
+    parser.add_argument('--webdriver-path', '-w', metavar='path', type=str, default='chromedriver',
+                        help='Chrome webdriver path. If not provided, it must be included in PATH env vars.')
 
-    parser.add_argument('--format',  metavar='o', type=str, default='csv', choices=['json', 'csv'],
-                        help='Whether to export as csv or json. Defaults to json')
-
-    parser.add_argument('--out',  metavar='o', type=str, default="data",
-                        help='Filename to save data.')
-
-    parser.add_argument('--webdriver-path', metavar='wp', type=str, default='chromedriver',
-                        help='Chrome webdriver path. If not provided, include webdriver in PATH env vars')
-
-    args = parser.parse_args()
+    args = parser.parse_args()  
 
     main(args)
