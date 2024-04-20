d359aa9043f21f40f5cb0fb2d78ab1f54d01bef1
Yuri Silveira <ypereirars@gmail.com>
2024-04-20 09:45:58 -0300

    Save the invoice after scraping

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import argparse
 from nfce.services import scrape_invoice
 from nfce.utils import save_json
+from nfce.database import save
 
 
-def main(url: str, output: str = None):
+def main(url: str, save_db: bool = True, output: str = None):
 
     invoice = scrape_invoice(url)
 
+    if save_db:
+        try:
+            print("Saving to database")
+            save(invoice)
+        except Exception as e:
+            print("Error saving to database")
+            print(e)
+
     if output and output.endswith(".json"):
+        print(f"Saving to file {output}")
         save_json(invoice, output)
 
     return invoice
@@ -25,7 +35,8 @@ if __name__ == "__main__":
         default=None,
         help="File path location where to save data. The file path must have a .json extension.",
     )
+    parser.add_argument("--save-db", action="store_true", help="Save to database")
 
     args = parser.parse_args()
 
-    main(args.url, args.output)
+    main(args.url, args.save_db, args.output)
