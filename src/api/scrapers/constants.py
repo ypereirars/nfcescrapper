1ade97da6aac85144e671087af28f8dc67b3324c
Yuri Silveira <ypereirars@gmail.com>
2024-05-18 18:55:31 -0300

    Fix constants

--- src/api/scrapers/constants.py
+++ src/api/scrapers/constants.py
@@ -1,9 +1,12 @@
-SPECIAL_CHARS = r"\(\)\n\t\r"
 UNWANTED_WORDS = [
     r"UN: *",
     r"Vl. Unit.:",
     r"Qtde.:",
     r"Código:",
     r"CNPJ:",
-    SPECIAL_CHARS.split(),
+    r"\(",
+    r"\)",
+    r"\n",
+    r"\r",
+    r"\t",
 ]
