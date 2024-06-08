326913e391268a75f4b8db90b9bfc82f529f9e19
Yuri Silveira <ypereirars@gmail.com>
2024-06-08 15:57:07 -0300

    Remove unecessary mdoules

--- src/scrapers/exceptions.py
+++
@@ -1,8 +0,0 @@
-class TimeoutException(Exception):
-    def __init__(
-        self,
-        timeout: int = 0,
-        message: str = "Timed out waiting {timout}s for page to load",
-    ):
-        self.timeout = timeout
-        super().__init__(message.format(timout=self.timeout))
